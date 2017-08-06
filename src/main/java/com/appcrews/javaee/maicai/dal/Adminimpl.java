395ac7eb2494bda1c6a2252a4538c14791bbb064
wyh <294634047@qq.com>
2017-08-06 17:55:00 +0800

    update

--- src/main/java/com/appcrews/javaee/maicai/dal/Adminimpl.java
+++ src/main/java/com/appcrews/javaee/maicai/dal/Adminimpl.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.appcrews.javaee.maicai.model.ShucaiInfo;
+import com.sun.xml.internal.ws.api.PropertySet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+
 
 public class Adminimpl extends JdbcDaoSupport implements Admin{
 	@SuppressWarnings({ "unchecked", "unused" })
